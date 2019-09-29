4e663e2fe3954cc034296e9c030c6ec114316466
Anik <anik@agiletech.com>
2019-09-29 11:53:10 -0400

    version 1 test scripts

---
+++ src/test/java/com/agileteach/test/mainTest.java
@@ -0,0 +1,15 @@
+package com.agileteach.test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class mainTest {
+
+	public static void main(String[] args) {
+		
+		
+		WebDriver driver=new ChromeDriver();
+
+	}
+
+}
